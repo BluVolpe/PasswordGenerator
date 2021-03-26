94bfec1e7b43965bacc95bca77a2142e4ac1f9c2
Blu VIII <34254871+BluVolpe@users.noreply.github.com>
2021-03-26 18:50:49 -0400

    created functions to retrieve random lower, upper, number, & symbols. Made array for all 4 functions

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,14 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
 
-  passwordText.value = password;
-
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  // const symbols = '!@#$%^&*()_+-=[]{}<>';
+  // return symbols [Math.floor(Math.random() * symbols.length)];
+
+     return String.fromCharCode(Math.floor(Math.random() * 14) + 33);
+}
+
+console.log(getRandomLower());
+console.log(getRandomUpper());
+console.log(getRandomNumber());
+console.log(getRandomSymbol());
+
+
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
