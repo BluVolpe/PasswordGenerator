1f9def29daa85b183c3d1e9cdd208a6fc96742cd
Blu VIII <34254871+BluVolpe@users.noreply.github.com>
2021-03-26 20:32:40 -0400

    Changed how to get the random numbers + added prompts for upper, lower, numeric, symbol

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,46 +1,37 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// The Password generator will provide a password with 8-50  characters based on criteria the user specifies.
+//Assignment Code + Event Listener to prompt questions when button pushed
+document.querySelector("#generate").addEventListener("click", writePassword);
+// Various Arrays 
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
+// Variable Declaration 
+var confirmLength;
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
 
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+// Prompt to confirm how many characters the user would like in their password
+function generatePassword() {
+  var confirmLength = parseInt(prompt("How many characters would you like your password to contain?"));
 
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+  // Loop if answer is outside the parameters 
+  while(confirmLength <= 7 || confirmLength >= 129) {
+      alert("Password length must be between 8-120 characters Try again");
+      var confirmLength = parseInt(prompt("How many characters would you like your password to contain?"));
+      } 
 
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+      // Repeat back how many charactes the user will have  
+      alert(`Your password will have ${confirmLength} characters`);
+    // Determine parameters of password 
+        var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");  
+        var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+        var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+        var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");   
+        } 
+     
 
-function getRandomSymbol() {
-  // const symbols = '!@#$%^&*()_+-=[]{}<>';
-  // return symbols [Math.floor(Math.random() * symbols.length)];
-
-     return String.fromCharCode(Math.floor(Math.random() * 14) + 33);
-}
-
-console.log(getRandomLower());
-console.log(getRandomUpper());
-console.log(getRandomNumber());
-console.log(getRandomSymbol());
-
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
